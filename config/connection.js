8a96dc390233a2f87341f2c57853dd981b271a24
Newgarden <jnewgarden92@gmail.com>
2019-05-15 18:11:49 -0500

    jaws

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,11 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        port: 8080,
         host: "localhost",
         user: "root",
         password: "",
         database: "burgers_db"
-    })
+    });
 };
 
 connection.connect(function (err) {
